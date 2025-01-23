619b54b552428e3215cb258146634b05529df092
Baptiste Pillot <baptiste@pillot.fr>
2025-01-23 17:35:24 +0100

    v0.0.2

--- src/vcs.ts
+++ src/vcs.ts
@@ -1,15 +1,11 @@
 #!/usr/bin/env node
 import appDir from '@itrocks/app-dir'
 import { accessSync, readdirSync, readFileSync, rmSync, writeFileSync } from 'fs'
-import { join } from 'path'
+import { basename, join } from 'path'
 import { execSync } from 'child_process'
 
 const itrocksPath = appDir + '/node_modules/@itrocks'
-const modules     = readdirSync(itrocksPath).filter(dir => !['.', '..', 'webstorm'].includes(dir))
-
-const vcsFile  = appDir + '/.idea/vcs.xml'
-let vcsContent = readFileSync(vcsFile, 'utf8')
-	.replaceAll(/\n\s*<mapping directory="\$PROJECT_DIR\$\/node_modules\/@itrocks\/.*" vcs="Git" \/>/g, '')
+const modules     = readdirSync(itrocksPath).filter(dir => !['.', '..'].includes(dir))
 
 modules.forEach(module => {
 	const path = join(itrocksPath, module)
@@ -19,8 +15,13 @@ modules.forEach(module => {
 	catch {
 		rmSync(path, { force: true, recursive: true })
 		execSync(`git clone git@github.com:itrocks-ts/${module}`, { cwd: itrocksPath, stdio: 'inherit' })
-		execSync(`npm run build`, { cwd: path, stdio: 'inherit' })
 	}
+})
+
+const vcsFile  = appDir + '/.idea/vcs.xml'
+let vcsContent = readFileSync(vcsFile, 'utf8')
+	.replaceAll(/\n\s*<mapping directory="\$PROJECT_DIR\$\/node_modules\/@itrocks\/.*" vcs="Git" \/>/g, '')
+modules.forEach(module => {
 	if (!vcsContent.includes(`/@itrocks/${module}"`)) {
 		vcsContent = vcsContent.replace(
 			'</component>',
@@ -28,7 +29,23 @@ modules.forEach(module => {
 		)
 	}
 })
-
 writeFileSync(vcsFile, vcsContent, 'utf8')
+
+const build = new Set<string>(modules)
+while (build.size) {
+	build.forEach(module => {
+		let   canBuild = true
+		const path     = join(itrocksPath, module)
+		Object.keys(JSON.parse(readFileSync(path + '/package.json', 'utf-8')).dependencies || {}).forEach(dependency => {
+			if (dependency.startsWith('@itrocks/') && build.has(basename(dependency))) {
+				canBuild = false
+			}
+		})
+		if (canBuild) {
+			execSync(`npm run build`, {cwd: path, stdio: 'inherit'})
+			build.delete(module)
+		}
+	})
+}
 
 console.log('Done.')
